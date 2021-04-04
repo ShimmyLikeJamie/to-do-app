760f0e851801ea1187d7c345b11185247edc6776
ShimmyLikeJamie <oppel.jamie@gmail.com>
2021-04-03 21:22:23 -0700

    Added App, DOM & elements objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const App = (() => {
+
+    const DOM = (() => {
+
+        let elements = {
+            appContainer: document.getElementById('appContainer'),
+            navBar:  document.getElementById('navBar'),
+            newProjectButton: document.getElementById('newProjectButton'),
+            taskContainer: document.getElementById('taskContainer'),
+            tasks: {}
+        }
+
+    });
+});
